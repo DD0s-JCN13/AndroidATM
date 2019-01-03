5546085ea405ebdd23e5e9ade14399aec2f0b42e
DD0s-JCN13 <jimmylin160@gmail.com>
2019-01-03 22:05:09 +0800

    Tester2 build tmp5

--- AndroidATM_II/app/src/main/java/com/dd0s/androidatm_ii/GenderActivity.java
+++ AndroidATM_II/app/src/main/java/com/dd0s/androidatm_ii/GenderActivity.java
@@ -2,12 +2,29 @@ package com.dd0s.androidatm_ii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GenderActivity extends AppCompatActivity {
+
+    EditText edGender;
+    RecyclerView rvGender;
+    TextView Genderrole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender);
+        findview();
+    }
+    public void findview(){
+        edGender = findViewById(R.id.ed_Gender);
+        rvGender = findViewById(R.id.recycler_Gender);
+        Genderrole = findViewById(R.id.gender_role);
+    }
+    public void next(View v){
+
     }
 }
